6cb143bfe23535477083424b2826a1e6d78e7bf6
Smenon2001 <Sushilmenon2001@gmail.com>
2022-01-21 00:37:23 -0800

    Update workload.tf

--- workload.tf
+++ workload.tf
@@ -1,7 +1,7 @@
 esource "aws_instance" "web_host" {
   # ec2 have plain text secrets in user data
   ami           = "${var.ami}"
-  instance_type = "t2.nono"
+  instance_type = "t2.nano"
 
   vpc_security_group_ids = [
   "${aws_security_group.web-node.id}"]
